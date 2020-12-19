ef7558f3ee15927f4bc348b9940d9eb1779e9e91
Nathaniel Hatfield <nthatfield@gmail.com>
2020-12-19 00:54:32 -0500

    completed part 1

--- Day 2 - Passwords.py
+++ Day 2 - Passwords.py
@@ -1,7 +1,12 @@
 day2_data = []
 #day2_part2 = []
 
-with open("/Users/nthatfield/Documents/Projects/Advent of Code 2020/day2_input.txt") as file:
+with open("/Users/nthatfield/Documents/Projects/Advent-of-Code-2020/day2_input.txt") as file:
+    total_valid_passwords = 0
     for line in file:
-        line = int(line.strip())
-        day2_data.append(line)
+        split_line = line.split()
+        password_bounds = split_line[0].split("-")
+        if split_line[2].count(split_line[1].rstrip(":")) >= int(password_bounds[0]) and split_line[2].count(split_line[1].rstrip(":")) <= int(password_bounds[1]):
+            total_valid_passwords = total_valid_passwords + 1
+
+print(total_valid_passwords)
