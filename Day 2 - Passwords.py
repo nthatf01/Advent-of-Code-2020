5b45a6a9ed383cba7f6f7d9c90f80494658bd224
Nathaniel Hatfield <nthatfield@gmail.com>
2020-12-18 23:15:48 -0500

    initial commit

---
+++ Day 2 - Passwords.py
@@ -0,0 +1,7 @@
+day2_data = []
+#day2_part2 = []
+
+with open("/Users/nthatfield/Documents/Projects/Advent of Code 2020/day2_input.txt") as file:
+    for line in file:
+        line = int(line.strip())
+        day2_data.append(line)
