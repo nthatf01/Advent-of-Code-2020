4d704432796589ed8dd7300b5c0ec5748cf18275
Nathaniel Hatfield <nthatfield@gmail.com>
2020-12-20 22:34:02 -0500

    completed part 1

--- Day 3 - Trees.py
+++ Day 3 - Trees.py
@@ -1,6 +1,16 @@
 day3_data = []
 #day3_part2 = []
+map_y = 0
+map_x = 0
+total_trees = 0
 
 with open("/Users/nthatfield/Documents/Projects/Advent-of-Code-2020/day3_input.txt") as file:
     for line in file:
-        print(line)
+        #print(map_y, map_x, map_x % 32)
+        #print(line[map_x % (len(line) - 1)])
+        if line[map_x % (len(line) - 1)] == '#':
+            total_trees = total_trees + 1
+        map_y = map_y + 1
+        map_x = map_x + 3
+
+print("The total number of trees is {strees}".format(strees = total_trees))
